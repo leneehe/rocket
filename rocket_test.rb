124d9ebb56a4d56bf837152d9c4bb07406d7d61f
Lena <fatmeow@gmail.com>
2017-12-08 15:31:31 -0500

    Test name and colour instance methods for Rocket, passed 6 so far

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,44 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+  def test_name_reader
+    rockie = Rocket.new(name: "Rocky")
+    expected = "Rocky"
+    actual = rockie.name
+    assert_equal(expected, actual)
+  end
+
+  def test_name_setter
+    rockie = Rocket.new
+    rockie.name = "Rocko"
+    expected = "Rocko"
+    actual = rockie.name
+    assert_equal(expected, actual)
+  end
+
+  def test_if_dont_assign_name_have_random_name
+    rockie = Rocket.new
+    random_name = rockie.name
+    refute_nil(random_name)
+  end
+
+  def test_colour_reader
+    rockie = Rocket.new(colour: "lavender")
+    expected = "lavender"
+    actual = rockie.colour
+    assert_equal(expected, actual)
+  end
+
+  def test_colour_setter
+    rockie = Rocket.new
+    rockie.colour = "black"
+    expected = "black"
+    actual = rockie.colour
+    assert_equal expected, actual
+  end
+
+  def test_if_not_set_colour_have_random_colour
+    rockie = Rocket.new
+    refute_nil(rockie.colour)
+  end
 end
